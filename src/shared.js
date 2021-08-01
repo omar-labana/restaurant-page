8982109de014e56e818f8a11971572c7291eef2b
Omar Labana <omarlabana@outlook.com>
2021-08-01 11:59:15 +0200

    Add shared generators

---
+++ src/shared.js
@@ -0,0 +1,22 @@
+const create = (elementName, styles = false, text = false) => {
+    let element = document.createElement(elementName)
+    if (styles) {
+        styles.forEach(style => {
+            element.classList.add(style)
+        });
+    }
+    if (text) {
+        element.textContent = text
+    }
+    return element
+}
+
+const clearView = () => {
+    document.getElementById('view').innerHTML = ''
+}
+
+const createFrame = () => {
+    return create('div', ['border-2', 'rounded-md', 'my-3', 'w-1/2', 'mx-auto', 'text-gray-100', 'text-xl'])
+}
+
+export { create, clearView, createFrame }
